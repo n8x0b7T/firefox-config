1a958267e446cb77e10d9da1c48b23e811ef1c3a
n8x0b7T <deretilium@protonmail.com>
2023-05-16 20:32:15 -0400

    set profiles correctly

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,22 +17,29 @@ if [ ! -d "/usr/lib/firefox" ]; then
     exit
 fi
 
-if [ -z "$1" ] || [ ! -f "$1/prefs.js" ]; then
-    echo "Please suply a valid firefox profile directory as the first positional argument"
-    exit
-fi
+# if [ -z "$1" ] || [ ! -f "$1/prefs.js" ]; then
+#     echo "Please suply a valid firefox profile directory as the first positional argument"
+#     exit
+# fi
 
-killall firefox 
 
-echo -e " \n\n\nChanging user profile"
-curl -s "https://raw.githubusercontent.com/n8x0b7T/user-overrides.js/main/user-overrides.js" > "$1/user-overrides.js"
-curl -s "https://raw.githubusercontent.com/arkenfox/user.js/master/updater.sh" | bash -s -- -p "$1" -s
 
 echo "Changing global configs"
 "$sudo" mkdir -p /usr/lib/firefox/defaults/pref
 "$sudo" bash -c 'curl -s "https://raw.githubusercontent.com/n8x0b7T/user-overrides.js/main/autoconfig.js" > /usr/lib/firefox/defaults/pref/autoconfig.js'
 "$sudo" bash -c 'curl -s "https://raw.githubusercontent.com/n8x0b7T/user-overrides.js/main/firefox.cfg" > /usr/lib/firefox/firefox.cfg'
 "$sudo" bash -c 'curl -s "https://raw.githubusercontent.com/n8x0b7T/user-overrides.js/main/policies.json" > /usr/lib/firefox/distribution/policies.json'
+
+rm -r ~/.mozilla
+firefox
+
+profile_dir=(~/.mozilla/firefox/*.default-release)
+echo "Your profile is $profile_dir"
+
+echo -e " \n\n\nChanging user profile"
+curl -s "https://raw.githubusercontent.com/n8x0b7T/user-overrides.js/main/user-overrides.js" > "$profile_dir/user-overrides.js"
+curl -s "https://raw.githubusercontent.com/arkenfox/user.js/master/updater.sh" | bash -s -- -p "$profile_dir" -s
+
 
 echo "Done."
 
