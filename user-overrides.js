ca9f98066dee027dfb6db114dd2c5e1bb8bf8f42
n8x0b7T <deretilium@protonmail.com>
2023-05-16 22:52:16 -0400

    fix dumb issues

--- user-overrides.js
+++ user-overrides.js
@@ -9,11 +9,6 @@ user_pref("browser.search.update", false);
 user_pref("browser.urlbar.quicksuggest.enabled", false);
 
 
-
-// load in cfg file for locking prefs
-pref("general.config.filename", "mozilla.cfg");
-lockPref("general.config.obscure_value", 0);
-
 user_pref("privacy.clearOnShutdown.history", false);
 user_pref("privacy.clearOnShutdown.downloads", false);
 
